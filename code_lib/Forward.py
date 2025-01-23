f999b0b71e9ba5512c37f585ca55665b29b8a74b
Efrain Magaña <emmt1998@gmail.com>
2025-01-23 16:40:39 -0300

    example pinns 1d & 2d

--- code_lib/Forward.py
+++ code_lib/Forward.py
@@ -2,6 +2,49 @@
 import jax as jx
 import jax.numpy as jnp
 
+def init_params(layers, key, init='glorot_normal', activation='relu', **kwargs):
+  #  https://pytorch.org/docs/stable/nn.init.html
+  Ws = []
+  bs = []
+  gain = {
+      'relu': jnp.sqrt(2),
+      'tanh': 5/3,
+      'selu': 3/4,
+  }[activation]
+    
+  key, subkey = jx.random.split(key)
+  fan_mode = 0 if kwargs.get('fan_mode', 'fan_in') else 1
+
+  for i in range(len(layers) - 1):
+    match init:
+      case 'glorot_normal':
+        std_glorot = jnp.sqrt(2/(layers[i] + layers[i + 1]))
+        Ws.append(jx.random.normal(subkey, (layers[i], layers[i + 1]))*std_glorot)
+        bs.append(jnp.zeros(layers[i + 1]))
+      case 'he_normal':
+        std_he = gain / jnp.sqrt(layers[i + fan_mode])
+        Ws.append(jx.random.normal(subkey, (layers[i], layers[i + 1]))*std_he)
+        bs.append(jnp.zeros(layers[i + 1]))
+      case 'uniform':
+        Ws.append(jx.random.uniform(subkey, (layers[i], layers[i+1])))
+        bs.append(jnp.zeros(layers[i + 1]))
+      case 'glorot_uniform':
+        bound = gain * jnp.sqrt(6 / layers[i + fan_mode])
+        Ws.append(jx.random.uniform(subkey, (layers[i], layers[i+1]), minval=-bound, maxval=bound))
+        bs.append(jnp.zeros(layers[i + 1]))
+      case 'he_uniform':
+        bound = gain * jnp.sqrt(3 / layers[i + fan_mode])
+        Ws.append(jx.random.uniform(subkey, (layers[i], layers[i+1]), minval=-bound, maxval=bound))
+        bs.append(jnp.zeros(layers[i + 1]))
+      case _:
+        raise NotImplementedError(
+            'Not available. Try:' +\
+            ' glorot_normal, glorot_uniform' +\
+            ', he_normal, he_uniform' +\
+            ', uniform.'
+        )
+  return [Ws, bs]
+
 @jx.jit
 def forward_pass_multi(H, params, s):
   # Forward pass with tanh
